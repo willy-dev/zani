344574b5dd1d1e05ffb15f6107ceb10b6bf9471b
Wilfred Marete <marete015@gmail.com>
2024-02-04 15:31:24 +0300

    starting nvabar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+      
+    </nav>
   )
 }
 
