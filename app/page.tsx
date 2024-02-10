4dbb3bcac2ae22f8ba3d825195b8e97c4e511845
Wilfred Marete <marete015@gmail.com>
2024-02-10 23:03:13 +0300

    added page section components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-teal-500 text-2xl">
-        Welcome to KayaNest!
-      </h1>
+      <>
+        
+      </>
       
     </div>
   );
