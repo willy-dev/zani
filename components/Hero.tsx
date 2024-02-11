706e7746c56013a9bf3b1d85dfad788ce38e8d21
Wilfred Marete <marete015@gmail.com>
2024-02-12 00:24:30 +0300

    hero section finished

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,8 +56,26 @@ const Hero = () => {
 
       {/* Right content */}
       <div className='relative flex flex-1 items-start'>
-        <div className='relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
+        <div className='relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
 
+          <div className='flex flex-col'>
+            <div className='flexBetween'>
+              <p className='regular-16 text-gray-20'>Location</p>
+              <Image src="/close.svg" alt='close' width={24} height={24} />
+            </div>
+            <p className='bold-20 text-white'>Rumuruti, Kenya</p>
+          </div>
+
+          <div className='flexBetween'>
+            <div className='flex flex-col'>
+              <p className='regular-16 block text-gray-20'>Distance</p>
+              <p className='bold-20 text-white'>173.28 mi</p>
+            </div>
+            <div className='flex flex-col'>
+              <p className='regular-16 block text-gray-20'>Elevation</p>
+              <p className='bold-20 text-white'>2.040 km</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
