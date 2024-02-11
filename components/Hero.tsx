51e1dcdcbedd690bbb9d9a72a2afd3f9f30ca184
Wilfred Marete <marete015@gmail.com>
2024-02-11 23:10:18 +0300

    hero section started

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+      <div className='hero-map' />
+
+      {/* Left content */}
+      <div className='relative z-20 flex flex-1  flex-col xl:w-1/2'>
+        <Image 
+          src='/camp.svg'
+          alt='camp'
+          width={50}
+          height={50}
+        />
+
+      </div>
+    </section>
   )
 }
 
