3cc4eb6de403f02d6c4d83643bc0739c85a3905a
Wilfred Marete <marete015@gmail.com>
2024-02-11 21:27:26 +0300

    added page section components to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <div>
       <>
-        
+        <Hero />
+        <Camp />
+        <Guide />
+        <Features />
+        <GetApp />
       </>
       
     </div>
