366551c86a3aa6fe854ec02d761d4b7d216249b5
Wilfred Marete <marete015@gmail.com>
2024-02-25 12:56:22 +0300

    completed scroll section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -71,7 +71,7 @@ const Camp = () => {
             <strong>Feeling lost</strong> on your journey to healthy, organic living?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
-            With care, dedication, and sustainable practices, we cultivate and harvest the freshest produce, ensuring every bite is filled with flavor and health.
+            With care, dedication, and sustainable practices, we cultivate and harvest the freshest produce, ensuring every bite is filled with flavor and health, bustle and muscle up.
           </p>
           <Image
             src='/quote.svg'
